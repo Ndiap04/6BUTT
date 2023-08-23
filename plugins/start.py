01029bdcea084c68c22c4cb07726b79d7e216b4c
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2023-08-23 12:45:28 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -127,22 +127,6 @@ async def start_command(client: Bot, message: Message):
                 )
             except BaseException:
                 pass
-    else:
-        out = start_button(client)
-        await message.reply_text(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=f"@{message.from_user.username}"
-                if message.from_user.username
-                else None,
-                mention=message.from_user.mention,
-                id=message.from_user.id,
-            ),
-            quote=True,
-            disable_web_page_preview=True,
-        )
-
-
     return
 
 
